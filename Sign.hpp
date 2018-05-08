8c41e5eeb6670925576eb61a1fe90dde6e649ec9
igilfu <33724031+igilfu@users.noreply.github.com>
2018-05-08 13:37:17 +0300

    Update Sign.hpp

--- Sign.hpp
+++ Sign.hpp
@@ -8,9 +8,17 @@ class Sign{
     Sign();
     
        operator char();
-    bool  operator==(const char& );
+   
     char GetSign() const;
    char& operator=(const char&);
    Sign&  operator=(const Sign& );
+      friend bool  operator==(const Sign& s,const char& );
    friend ostream& operator<<(ostream& os, Sign const& );
+};
+
+inline bool  operator==(const Sign& s,const char& c){
+
+  return s.GetSign()==c;
+   
+}
 };
