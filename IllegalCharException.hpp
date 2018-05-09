3e058db48280cf49d3a93824737408722e32b5ab
igilfu <33724031+igilfu@users.noreply.github.com>
2018-05-09 19:30:16 +0300

    Update IllegalCharException.hpp

--- IllegalCharException.hpp
+++ IllegalCharException.hpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 class IllegalCharException{
+/*
+*Declarence of varible
+*/        
     char c;
 public:
+/*
+*Declarence on function
+*/     
     IllegalCharException(const char& c);
     char theChar() const;
 };
