8217a6abe107cc4e70caae7b965c514538f943f9
igilfu <33724031+igilfu@users.noreply.github.com>
2018-05-09 19:29:30 +0300

    Update IllegalCharException.cpp

--- IllegalCharException.cpp
+++ IllegalCharException.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "IllegalCharException.hpp"
 using namespace std;
-
+/*
+*Retuern the illegal char
+*/
 IllegalCharException::IllegalCharException(const char& c){
     this->c=c;
 }
