e95f9ee34ce97115612eabb7367058fced89e945
igilfu <33724031+igilfu@users.noreply.github.com>
2018-05-09 19:36:40 +0300

    Update Sign.cpp

--- Sign.cpp
+++ Sign.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include "Sign.hpp"
 using namespace std;
+/*
+*Constructor
+*/ 
 Sign::Sign(const char& n){
   this->a=n;
 }
+/*
+*Default Constructor
+*/
 Sign::Sign(){
   this->a='.';
 }
-
+/*
+*Geter
+*/
 char Sign::GetSign() const{
  return this->a;
 }
-
+/*
+*Opreator input to Change the char in the Sign 
+*/
 Sign& Sign:: operator=(const char& c){
 if(c!='X'&&c!='O'&&c!='.')
   throw IllegalCharException(c);
@@ -19,6 +29,9 @@ else
   this->a=c;
 return *this;
 }
+/*
+*Opreator input to Change Sign with other sign
+*/
 Sign& Sign::operator=(const Sign& c)
 {
   if(c.GetSign()!='X'&&c.GetSign()!='O'&&c.GetSign()!='.')
@@ -27,11 +40,17 @@ Sign& Sign::operator=(const Sign& c)
     this->a=c.GetSign();
   return *this;
 }
+/*
+*print sign
+*/
 ostream& operator<<(ostream& os, Sign const& b)
 {
     os << b.a;
     return os;
 }
+/*
+*Opreator to create Sign insted char
+*/
 Sign::operator char()
 {
  
