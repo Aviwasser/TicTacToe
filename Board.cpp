59e2a1f1a441cad9853bbcc20a015c4ce9677f7b
IdanHol <33767899+IdanHol@users.noreply.github.com>
2018-05-09 21:01:27 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+/*
+*Default Constructor
+*/
 Board::Board(int len){
     this->len=len;
     this->brd=new Sign*[len];
@@ -9,6 +12,9 @@ Board::Board(int len){
         this->brd[i]=new Sign[len];
     }
 }
+/*
+*Copy Constructor
+*/
 Board::Board(const Board& other){
     this->len=other.len;
     this->brd=new Sign*[other.len];
@@ -19,11 +25,17 @@ Board::Board(const Board& other){
         }
     }
 }
+/*
+*Opreator to return Sign in Some Cooridnate
+*/
 Sign& Board:: operator[](const vector<int> v){
     if(v[0]>=0 && v[0]<len && v[1]>=0 && v[1]<len)
         return brd[v[0]][v[1]];
     throw IllegalCoordinateException (v[0],v[1]);
 }
+/*
+*Opreator input to Change Sign in the Board
+*/
 Board& Board:: operator=(const char& c){
         for(int i=0;i<len;++i){
             for(int j=0;j<len;++j){
@@ -32,6 +44,9 @@ Board& Board:: operator=(const char& c){
         }
 return *this;
 }
+/*
+*Operator input to change the Board to other
+*/
 Board& Board:: operator=(const Board& b){
     this->~Board();
     this->len=b.len;
@@ -44,6 +59,9 @@ Board& Board:: operator=(const Board& b){
     }
     return *this;
 }
+/*
+*Destractor
+*/
 Board::~Board(){
     for(int i=0;i<this->len;++i){
         delete[] brd[i];
