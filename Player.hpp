5207285690eab487ab4b28bd3a37417276caa35c
IdanHol <33767899+IdanHol@users.noreply.github.com>
2018-05-16 01:52:45 +0300

    Delete Player.hpp

--- Player.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-//#include "Coordinate.hpp"
-#include "Board.h"
-using namespace std;
- class Player {
-    public:
-     const string name();
-     virtual const Coordinate play(const Board&);
-};
