6dff2052e751ed503958a5fb54c4fc731d44773e
IdanHol <33767899+IdanHol@users.noreply.github.com>
2018-05-21 22:19:00 +0300

    Winning all

--- Champion.cpp
+++ Champion.cpp
@@ -2,19 +2,33 @@
 
 
 const Coordinate Champion::play(const Board& board){
-   unsigned int i=0,j=board.size()-1,size=j;
-    Coordinate c{i,j};
-   if(board[c]=='.')
-   return c;
-     c={j,i};
+   unsigned int i=0,j=0,size=board.size()-1;
+    //cout<<"h00"<<endl;
+//     Coordinate c{i,j};
+//    if(board[c]=='.')
+//    return c;
+   
+     Coordinate c={size,j};
      if(board[c]=='.')
    return c;
+       c={i,size};
+     if(board[c]=='.')
+   return c;
+
+   c={i+1,size-1};
+     if(board[c]=='.')
+   return c;
+
+   c={size-1,j+1};
+     if(board[c]=='.')
+   return c;
+
    for(i = 1; i < size; i++){
-          c={i,j};
+          c={size,i};
          if(board[c]=='.')
              return c;
     }
-    
+   
     for(i = 0; i < size; i++){
         for(j = 0; j < size; j++){
             c={i,j};
@@ -22,5 +36,6 @@ const Coordinate Champion::play(const Board& board){
              return c;
         }
     }
+    
     return {0,0};
 }
