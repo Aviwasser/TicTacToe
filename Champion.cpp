1ee6fae68a8b81ac25dd6902ee0394e1b06ed51f
igilfu <33724031+igilfu@users.noreply.github.com>
2018-05-21 21:43:41 +0300

    Update Champion.cpp

--- Champion.cpp
+++ Champion.cpp
@@ -2,5 +2,25 @@
 
 
 const Coordinate Champion::play(const Board& board){
+   unsigned int i=0,j=board.size()-1,size=j;
+    Coordinate c{i,j};
+   if(board[c]=='.')
+   return c;
+     c={j,i};
+     if(board[c]=='.')
+   return c;
+   for(i = 1; i < size; i++){
+          c={i,j};
+         if(board[c]=='.')
+             return c;
+    }
     
+    for(i = 0; i < size; i++){
+        for(j = 0; j < size; j++){
+            c={i,j};
+             if(board[c]=='.')
+             return c;
+        }
+    }
+    return {0,0};
 }
