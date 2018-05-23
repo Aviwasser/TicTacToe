6f0355eb0b79e72d1257308b6014fafe3f567fc6
igilfu <33724031+igilfu@users.noreply.github.com>
2018-05-23 14:41:25 +0300

    Update TicTacToe.h

--- TicTacToe.h
+++ TicTacToe.h
@@ -2,18 +2,20 @@
 
 #include "DummyPlayers.h"
 class TicTacToe  {
+  /*
+  *Declarence of varible
+  */ 
   private:
     Board brd;
     Player* win;
     bool check(Coordinate c);
+   /*
+   *Declarence on function
+   */ 
     public:
         TicTacToe(int);
         void print();
         Board board() const;
         void play(Player&,Player&);
         const Player& winner() const;
-        //   cout<<"sdffdsfsd";
-        //   Player *x;
-        //   //x=(YXPlayer*)malloc(sizeof(YXPlayer));
-        // return x;
 };
