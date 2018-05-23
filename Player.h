2f43e901d177e3cf84e8f05790a04396507cd91f
igilfu <33724031+igilfu@users.noreply.github.com>
2018-05-23 14:46:19 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -5,17 +5,16 @@
 #include "Board.h"
 using namespace std;
  class Player {
+/*
+*Declarence of varible
+*/   
     public:
     char myChar;
+/*
+*Declarence on function
+*/     
      virtual const string name()const{return "Player";}
      virtual const Coordinate play(const Board& Board)=0;
      const char getChar() const;
      void setChar(char c);
-    //  Player& operator=(const Player& other){
-    //      if(other.name().compare("XYPlayer")==0)
-    //      *this->name= "XYPlayer";
-    //      else *this->name= "YXPlayer";
-    //      return *this;
-         
-    //  }
 };
