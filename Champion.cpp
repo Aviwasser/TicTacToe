2091bcddc43b9b9950ad1f9b1004ffc1d2c6c16b
igilfu <33724031+igilfu@users.noreply.github.com>
2018-05-23 14:38:23 +0300

    Update Champion.cpp

--- Champion.cpp
+++ Champion.cpp
@@ -1,9 +1,11 @@
 #include "Champion.h"
-
+/**
+*Strategy to win the other player
+*/
 
 const Coordinate Champion::play(const Board& board){
    uint i=0,j=0,size=board.size()-1;
-   
+   //check the corrner
      Coordinate c={size,j};
      if(board[c]=='.')
    return c;
@@ -18,7 +20,7 @@ const Coordinate Champion::play(const Board& board){
    c={size-1,j+1};
      if(board[c]=='.')
    return c;
-
+   //run on all the board
    for(i = 1; i <= size; i++){
           c={size,i};
          if(board[c]=='.')
